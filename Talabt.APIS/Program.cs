b7bb178a653f74714b35f222ab556b9d75dbcdbe
Esraa <esraasherif1042@gmail.com>
2024-10-22 12:07:20 +0200

    Part 01 ASP.NET Core Web APIs - Project Structure

--- Talabt.APIS/Program.cs
+++ Talabt.APIS/Program.cs
@@ -6,16 +6,20 @@ namespace Talabt.APIS
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+
+            #region Configure Services-Add services to the container.
+
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(); 
+            #endregion
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            #region Configure- Configure the HTTP request pipeline.
+
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -27,7 +31,8 @@ namespace Talabt.APIS
             app.UseAuthorization();
 
 
-            app.MapControllers();
+            app.MapControllers(); 
+            #endregion
 
             app.Run();
         }
