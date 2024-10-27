f4d198b250fa53687d4f39ae1f9c951a85acb9c9
Esraa <esraasherif1042@gmail.com>
2024-10-27 18:23:41 +0200

    Part 04 Employee Example Get Employees  - Draft

---
+++ Talabat.Core/Specifications/EmployeeWithDepartSpecif.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+
+namespace Talabat.Core.Specifications
+{
+    public class EmployeeWithDepartSpecif:BaseSpecification<Employee>
+    {
+        public EmployeeWithDepartSpecif()
+        {
+            Include.Add(e => e.Department);
+        }
+    }
+}
---
+++ Talabt.APIS/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.Core.Entities;
+using Talabat.Core.Repositories;
+using Talabat.Core.Specifications;
+
+namespace Talabt.APIS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IGenericRepository<Employee> _emploRepo;
+
+        public EmployeeController(IGenericRepository<Employee> EmploRepo)
+        {
+            _emploRepo = EmploRepo;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+        {
+            var spec = new EmployeeWithDepartSpecif();
+            var employee=await _emploRepo.GetAllWithSpecAsync(spec);
+            return Ok(employee);
+        }
+    }
+}
