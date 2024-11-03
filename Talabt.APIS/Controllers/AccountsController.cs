5e2dcac6dbc2f1f5ee3929cd21b8f63b899a64e7
Esraa <esraasherif1042@gmail.com>
2024-11-03 13:07:14 +0200

    Part 09 Login Endpoint

---
+++ Talabt.APIS/Controllers/AccountsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.Core.Entities.identity;
+using Talabt.APIS.DTO;
+using Talabt.APIS.Errors;
+
+namespace Talabt.APIS.Controllers
+{
+    
+    public class AccountsController : APIBaseController
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AccountsController(UserManager<AppUser> userManager)
+        {
+            this._userManager = userManager;
+        }
+
+        //register
+        [HttpPost("Register")]
+        public async Task<ActionResult<UserDTO>> Register(RegisterDTO model)
+        {
+            
+            
+            var User = new AppUser()
+            {
+                DisplayName = model.DisplayName,
+                Email = model.Email,
+                UserName = model.Email.Split('@')[0],
+                PhoneNumber = model.PhoneNumber,
+            };
+           var result=  await  _userManager.CreateAsync(User, model.Password);
+            if (!result.Succeeded) return BadRequest(new ApiResponse(400));
+
+            var returneduser = new UserDTO()
+            {
+                DisplayName = User.DisplayName,
+                Email = User.Email,
+                Token = "this will be token"
+            };
+            return Ok(returneduser);
+        
+        }
+
+
+        //login
+
+
+
+
+
+    }
+}
---
+++ Talabt.APIS/DTO/RegisterDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Talabt.APIS.DTO
+{
+    public class RegisterDTO
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string DisplayName { get; set; }
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }
+        [Required]
+        [RegularExpression("(?=^.{6,10}$)(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&amp;*()_+]).*$",
+            ErrorMessage ="Password must contain 1Uppercase,1Lowercase,1 Digit,1 Spaecial Character")]
+        public string Password { get; set; }
+
+    }
+}
