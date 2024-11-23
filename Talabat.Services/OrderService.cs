373287c1727fd8329dd110ad954538fd2a3f54a4
Esraa <esraasherif1042@gmail.com>
2024-11-23 11:25:00 +0200

    Part 01 Order Service - Interface

---
+++ Talabat.Services/OrderService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities.Core_Aggra;
+using Talabat.Core.Entities.Order_Aggra;
+using Talabat.Core.Services;
+
+namespace Talabat.Services
+{
+    public class OrderService : IOrderServices
+    {
+        public Task<Order> CreateOrderAsync(string BuyerEmail, string BasketId, int DeliveryMethodId, Address shoppingAddress)
+        {
+        }
+
+        public Task<Order> GetOrderByIdWithSpecificUserAsync(string BuyerEmail, int orderId)
+        {
+        }
+
+        public Task<IReadOnlyList<Order>> GetOrderWithSpecificUserAsync(string BuyerEmail)
+        {
+        }
+    }
+}
